c9300bf2614dc97d2cab63f028142999b015167f
Sushant <sunsinghny07@gmail.com>
2026-05-02 22:30:25 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,9 +7,11 @@ def main():
 
     # TODO: Uncomment the code below to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
+        connection, address = server_socket.accept() # wait for client
+        print(f"Connected to {address}")
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        connection.sendall(response.encode("utf-8"))
 
 if __name__ == "__main__":
     main()
