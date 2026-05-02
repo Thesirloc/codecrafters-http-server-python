84ee51f5b9a95ee8b511bf99038081473e30440b
Sushant <sunsinghny07@gmail.com>
2026-05-02 22:55:38 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,10 +7,20 @@ def main():
 
     # TODO: Uncomment the code below to pass the first stage
     #
-    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
+    reuse_port = hasattr(socket, "SO_REUSEPORT")
+    with socket.create_server(("localhost", 4221), reuse_port=reuse_port) as server_socket:
         connection, address = server_socket.accept() # wait for client
-        print(f"Connected to {address}")
-        response = "HTTP/1.1 200 OK\r\n\r\n"
+        print(f"Client connected at {address}")
+        data = connection.recv(1024)
+        print(f"Received data: {data.decode('utf-8')}")
+        
+        #Extract the path from the request line
+        request_line = data.decode('utf-8').split('\r\n')[0]
+        method, path, version = request_line.split()
+        if path == "/":
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
         connection.sendall(response.encode("utf-8"))
 
 if __name__ == "__main__":
